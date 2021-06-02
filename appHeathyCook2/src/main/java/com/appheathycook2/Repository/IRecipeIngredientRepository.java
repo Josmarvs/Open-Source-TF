9a05c5fdd11b22736a42166c1473c58be7fd486f
AntonioFerrandiz <64290206+AntonioFerrandiz@users.noreply.github.com>
2021-06-01 17:11:39 -0700

    Add files via upload

---
+++ appHeathyCook2/src/main/java/com/appheathycook2/Repository/IRecipeIngredientRepository.java
@@ -0,0 +1,11 @@
+package com.appheathycook2.Repository;
+
+import com.appheathycook2.Entity.RecipeIngredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface IRecipeIngredientRepository extends JpaRepository<RecipeIngredient,Long>{
+    public List<RecipeIngredient> findByQuantity(Integer quantity);
+}
