787153714f47abedf3ac2ab0b77f0c89b0e1b119
Josmarvs <55904221+Josmarvs@users.noreply.github.com>
2021-06-28 15:54:08 -0500

    0

--- appHeathyCook2/src/main/java/com/appheathycook2/Service/IRecipeIngredientService.java
+++
@@ -1,9 +0,0 @@
-package com.appheathycook2.Service;
-
-import com.appheathycook2.Entity.RecipeIngredient;
-
-import java.util.List;
-
-public interface IRecipeIngredientService extends CrudService<RecipeIngredient>{
-    public List<RecipeIngredient> findByQuantity(Integer quantity)throws Exception;
-}
